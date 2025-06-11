989e86a68d8bebf5b33d05a43e152be8610f5dd7
Asaf Ben Artzy <asaf.benartzi@wsc-sprorts.com>
2025-06-11 11:20:55 +0300

    widgets sample titles update

--- samples/widgets/src/main/java/com/wscsports/blaze_sample_android/samples/widgets/WidgetScreenType.kt
+++ samples/widgets/src/main/java/com/wscsports/blaze_sample_android/samples/widgets/WidgetScreenType.kt
@@ -7,37 +7,37 @@ enum class WidgetScreenType(
     val navDestinationId: Int
 ) {
     STORIES_ROW(
-        title = "Stories-row",
+        title = "Stories row",
         description = "This represents a Blaze widget view for stories with a row layout on a horizontal axis",
         navDestinationId = R.id.action_navigation_widget_list_to_storiesRowFragment
     ),
     STORIES_GRID(
-        title = "Stories-grid",
+        title = "Stories grid",
         description = "This represents a Blaze widget view for stories with a grid layout on a vertical axis",
         navDestinationId = R.id.action_navigation_widget_list_to_storiesGridFragment
     ),
     MOMENTS_ROW(
-        title = "Moments-row",
+        title = "Moments row",
         description = "This represents a Blaze widget view for moments with a row layout on a horizontal axis",
         navDestinationId = R.id.action_navigation_widget_list_to_momentsRowFragment
     ),
     MOMENTS_GRID(
-        title = "Moments-grid",
+        title = "Moments grid",
         description = "This represents a Blaze widget view for moments with a grid layout on a vertical axis",
         navDestinationId = R.id.action_navigation_widget_list_to_momentsGridFragment
     ),
     VIDEOS_ROW(
-        title = "Videos-row",
+        title = "Videos row",
         description = "This represents a Blaze widget view for videos with a row layout on a horizontal axis",
         navDestinationId = R.id.action_navigation_widget_list_to_videosRowFragment
     ),
     VIDEOS_GRID(
-        title = "Videos-grid",
+        title = "Videos grid",
         description = "This represents a Blaze widget view for videos with a grid layout on a horizontal axis",
         navDestinationId = R.id.action_navigation_widget_list_to_videosGridFragment
     ),
     MIXED_WIDGETS(
-        title = "Mixed-widgets",
+        title = "Widgets feed demo",
         description = "This represents a feed with multiple widgets on the same screen with refresh functionality",
         navDestinationId = R.id.action_navigation_widget_list_to_mixedWidgetsFragment
     );

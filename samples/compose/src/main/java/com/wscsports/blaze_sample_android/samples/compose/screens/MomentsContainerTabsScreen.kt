830be24ca9d3e27a5f9f1324c0336a4fec38c9e3
Tuval Barak <79657230+tuvalbarak@users.noreply.github.com>
2025-08-28 13:18:52 +0300

    Implemented Moments Container Tabs sample for Native and Compose (#1)
    Implemented Moments Container Tabs sample for Native and Compose

---
+++ samples/compose/src/main/java/com/wscsports/blaze_sample_android/samples/compose/screens/MomentsContainerTabsScreen.kt
@@ -0,0 +1,82 @@
+package com.wscsports.blaze_sample_android.samples.compose.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.blaze.blazesdk.data_source.BlazeDataSourceType
+import com.blaze.blazesdk.data_source.BlazeWidgetLabel
+import com.blaze.blazesdk.features.moments.container.tabs.compose.BlazeMomentsPlayerContainerTabsCompose
+import com.blaze.blazesdk.features.moments.container.tabs.compose.BlazeMomentsPlayerContainerTabsComposeStateHandler
+import com.blaze.blazesdk.features.moments.container.tabs.models.BlazeMomentsContainerTabItem
+import com.blaze.blazesdk.style.players.BlazePlayerButtonCustomImageStates
+import com.blaze.blazesdk.style.players.moments.BlazeMomentsPlayerStyle
+import com.blaze.blazesdk.style.players.tabs.BlazePlayerTabsStyle
+import com.wscsports.blaze_sample_android.core.Constants.MOMENTS_CONTAINER_TABS_1_DEFAULT_LABEL
+import com.wscsports.blaze_sample_android.core.Constants.MOMENTS_CONTAINER_TABS_2_DEFAULT_LABEL
+import com.wscsports.blaze_sample_android.core.MomentsContainerTabsDelegateImpl
+import com.wscsports.blaze_sample_android.samples.compose.ComposeViewModel
+
+
+private const val MOMENTS_CONTAINER_TABS_SOURCE_ID = "compose-container-moments-tabs"
+private const val MOMENTS_CONTAINER_TABS_COMPOSE_ID_1 = "tabs-compose-1"
+private const val MOMENTS_CONTAINER_TABS_COMPOSE_ID_2 = "tabs-compose-2"
+
+@Composable
+fun MomentsContainerTabsScreen(viewModel: ComposeViewModel) {
+    val momentsContainerTabsStateHandler = remember {
+        BlazeMomentsPlayerContainerTabsComposeStateHandler(
+            containerSourceId = MOMENTS_CONTAINER_TABS_SOURCE_ID,
+            containerTabsDelegate = MomentsContainerTabsDelegateImpl(),
+            tabs = listOf(
+                BlazeMomentsContainerTabItem(
+                    containerId = MOMENTS_CONTAINER_TABS_COMPOSE_ID_1,
+                    title = "Trending",
+                    dataSource = BlazeDataSourceType.Labels(BlazeWidgetLabel.singleLabel(MOMENTS_CONTAINER_TABS_1_DEFAULT_LABEL)),
+                    icon = BlazePlayerButtonCustomImageStates(
+                        imageSelectedPathResId = com.wscsports.blaze_sample_android.core.ui.R.drawable.ic_tabs_trending,
+                        imageUnselectedPathResId = com.wscsports.blaze_sample_android.core.ui.R.drawable.ic_tabs_trending,
+                    )
+                ),
+                BlazeMomentsContainerTabItem(
+                    containerId = MOMENTS_CONTAINER_TABS_COMPOSE_ID_2,
+                    title = "For You",
+                    dataSource = BlazeDataSourceType.Labels(BlazeWidgetLabel.singleLabel(MOMENTS_CONTAINER_TABS_2_DEFAULT_LABEL)),
+                    icon = BlazePlayerButtonCustomImageStates(
+                        imageSelectedPathResId = com.wscsports.blaze_sample_android.core.ui.R.drawable.ic_tabs_for_you,
+                        imageUnselectedPathResId = com.wscsports.blaze_sample_android.core.ui.R.drawable.ic_tabs_for_you,
+                    )
+                ),
+            ),
+            playerStyle = BlazeMomentsPlayerStyle.base().apply { 
+                buttons.exit.isVisible = false
+            },
+            tabsStyle = BlazePlayerTabsStyle.base()
+        )
+    }
+
+    LaunchedEffect(viewModel) {
+        viewModel.onVolumeChangedEvent.collect {
+            momentsContainerTabsStateHandler.onVolumeChanged()
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF000000))
+    ) {
+        BlazeMomentsPlayerContainerTabsCompose(
+            modifier = Modifier
+                .statusBarsPadding()
+                .fillMaxSize(),
+            stateHandler = momentsContainerTabsStateHandler,
+        )
+    }
+
+}
--- samples/compose/src/main/java/com/wscsports/blaze_sample_android/samples/compose/screens/WidgetsFeedScreen.kt
+++ samples/compose/src/main/java/com/wscsports/blaze_sample_android/samples/compose/screens/WidgetsFeedScreen.kt
@@ -10,21 +10,63 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.blaze.blazesdk.data_source.BlazeDataSourceType
+import com.blaze.blazesdk.data_source.BlazeWidgetLabel
+import com.blaze.blazesdk.features.moments.widgets.compose.BlazeComposeWidgetMomentsStateHandler
 import com.blaze.blazesdk.features.moments.widgets.compose.row.BlazeComposeMomentsWidgetRowView
+import com.blaze.blazesdk.features.stories.widgets.compose.BlazeComposeWidgetStoriesStateHandler
 import com.blaze.blazesdk.features.stories.widgets.compose.grid.BlazeComposeStoriesWidgetGridView
 import com.blaze.blazesdk.features.stories.widgets.compose.row.BlazeComposeStoriesWidgetRowView
+import com.blaze.blazesdk.style.players.moments.BlazeMomentsPlayerStyle
+import com.blaze.blazesdk.style.players.stories.BlazeStoryPlayerStyle
+import com.blaze.blazesdk.style.widgets.BlazeWidgetLayout
+import com.wscsports.blaze_sample_android.core.Constants.MOMENTS_WIDGET_DEFAULT_LABEL
+import com.wscsports.blaze_sample_android.core.Constants.STORIES_WIDGET_DEFAULT_LABEL
+import com.wscsports.blaze_sample_android.core.WidgetDelegateImpl
 import com.wscsports.blaze_sample_android.samples.compose.ComposeTopBar
 import com.wscsports.blaze_sample_android.samples.compose.ComposeViewModel
 
 @Composable
 fun WidgetsFeedScreen(
-    viewModel: ComposeViewModel,
     onTopBarBackPressed: () -> Unit,
-    modifier: Modifier = Modifier
 ) {
     val scrollState = rememberScrollState()
+
+    val composeWidgetDelegate = remember { WidgetDelegateImpl() }
+    
+    val storiesRowStateHandler = remember {
+        BlazeComposeWidgetStoriesStateHandler(
+            widgetId = "compose-stories-row-widget-id",
+            widgetLayout = BlazeWidgetLayout.Presets.StoriesWidget.Row.circles,
+            playerStyle = BlazeStoryPlayerStyle.base(),
+            dataSourceType = BlazeDataSourceType.Labels(BlazeWidgetLabel.singleLabel(STORIES_WIDGET_DEFAULT_LABEL)),
+            widgetDelegate = composeWidgetDelegate
+        )
+    }
+
+    val momentsRowStateHandler = remember {
+        BlazeComposeWidgetMomentsStateHandler(
+            widgetId = "compose-moments-row-widget-id",
+            widgetLayout = BlazeWidgetLayout.Presets.MomentsWidget.Row.verticalAnimatedThumbnailsRectangles,
+            playerStyle = BlazeMomentsPlayerStyle.base(),
+            dataSourceType = BlazeDataSourceType.Labels(BlazeWidgetLabel.singleLabel(MOMENTS_WIDGET_DEFAULT_LABEL)),
+            widgetDelegate = composeWidgetDelegate
+        )
+    }
+
+    val storiesGridStateHandler = remember {
+        BlazeComposeWidgetStoriesStateHandler(
+            widgetId = "compose-stories-Grid-widget-id",
+            widgetLayout = BlazeWidgetLayout.Presets.StoriesWidget.Grid.twoColumnsVerticalRectangles,
+            playerStyle = BlazeStoryPlayerStyle.base(),
+            dataSourceType = BlazeDataSourceType.Labels(BlazeWidgetLabel.singleLabel(STORIES_WIDGET_DEFAULT_LABEL)),
+            widgetDelegate = composeWidgetDelegate
+        )
+    }
+    
     Column {
         ComposeTopBar(
             title = "Compose",
@@ -36,7 +78,7 @@ fun WidgetsFeedScreen(
                 modifier = Modifier
                     .height(140.dp)
                     .fillMaxWidth(),
-                widgetStoriesStateHandler = viewModel.storiesRowStateHandler
+                widgetStoriesStateHandler = storiesRowStateHandler
             )
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = "Compose Moments Row", modifier = Modifier.padding(16.dp))
@@ -44,7 +86,7 @@ fun WidgetsFeedScreen(
                 modifier = Modifier
                     .height(300.dp)
                     .fillMaxWidth(),
-                widgetMomentsStateHandler = viewModel.momentsRowStateHandler
+                widgetMomentsStateHandler = momentsRowStateHandler
             )
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = "Compose Stories Grid ", modifier = Modifier.padding(16.dp))
@@ -52,7 +94,7 @@ fun WidgetsFeedScreen(
                 modifier = Modifier
                     .fillMaxHeight()
                     .fillMaxWidth(),
-                widgetStoriesStateHandler = viewModel.storiesGridStateHandler
+                widgetStoriesStateHandler = storiesGridStateHandler
             )
         }
     }

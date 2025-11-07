5df454f73c7d4d25cdc3abb59e8434a5da06b9f2
Joshua Rabanal <joshua.rabanal@yinzcam.com>
2025-11-07 11:55:38 -0500

    moment not launching - removed callback at request of 3rd party

--- app/src/main/java/com/wscsports/blaze_sample_android/MainActivity.kt
+++ app/src/main/java/com/wscsports/blaze_sample_android/MainActivity.kt
@@ -23,9 +23,7 @@ class MainActivity : AppCompatActivity() {
             BlazeSDK.playMoment(
                 "nbl",
                 BlazeMomentsPlayerStyle.base()
-            ){
-                BlazeSDK.dismissCurrentPlayer()
-            }
+            )
         }
     }
 

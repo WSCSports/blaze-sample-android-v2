802c142606ec7dbe07a10c8c16eac5d13937a7cd
tuvalbarak <baraktuval@gmail.com>
2026-07-20 17:14:06 +0300

    Data sources samples to Widgets screen

--- samples/widgets/src/main/java/com/wscsports/blaze_sample_android/samples/widgets/screens/StoriesRowFragment.kt
+++ samples/widgets/src/main/java/com/wscsports/blaze_sample_android/samples/widgets/screens/StoriesRowFragment.kt
@@ -2,8 +2,6 @@ package com.wscsports.blaze_sample_android.samples.widgets.screens
 
 import android.graphics.Color
 import android.util.TypedValue
-import com.blaze.blazesdk.data_source.BlazeDataSourceType
-import com.blaze.blazesdk.data_source.BlazeWidgetLabel
 import com.blaze.blazesdk.extentions.blazeDeepCopy
 import com.blaze.blazesdk.style.shared.models.BlazeObjectXPosition
 import com.blaze.blazesdk.style.shared.models.BlazeObjectYPosition
@@ -40,11 +38,9 @@ class StoriesRowFragment: BaseWidgetFragment(R.layout.fragment_stories_row) {
         // The custom layout can also be set during initWidget, rather than using updateWidgetLayout.
         // In this case, we are setting the layout to some default preset.
         val widgetLayout = viewModel.getWidgetLayoutBasePreset()
-        val dataState = viewModel.getCurrWidgetDataState()
-        val dataSource = BlazeDataSourceType.Labels(
-            blazeWidgetLabel = BlazeWidgetLabel.singleLabel(dataState.labelName),
-            orderType = dataState.orderType,
-        )
+        // The data source is built from the state selected in the "Edit data source"
+        // bottom sheet - see WidgetDataState.toDataSource() for all the examples.
+        val dataSource = viewModel.getCurrWidgetDataState().toDataSource()
         binding.storiesRowWidgetView.initWidget(
             widgetLayout = widgetLayout,
             dataSource = dataSource,
@@ -78,11 +74,7 @@ class StoriesRowFragment: BaseWidgetFragment(R.layout.fragment_stories_row) {
     }
 
     override fun onNewDatasourceState(dataState: WidgetDataState) {
-        val dataSource = BlazeDataSourceType.Labels(
-            blazeWidgetLabel = BlazeWidgetLabel.singleLabel(dataState.labelName),
-            orderType = dataState.orderType,
-        )
-        binding.storiesRowWidgetView.updateDataSource(dataSource, false)
+        binding.storiesRowWidgetView.updateDataSource(dataState.toDataSource(), false)
     }
 
     // For rectangle preset, we give the widget a height of 180dp, and for circle preset, we give it a height of 160dp.
